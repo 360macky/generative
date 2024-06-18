9c6b08cc472c35b49e53a94a7c198320814245f2
Marcelo Arias <mail.marcelo.as@gmail.com>
2024-06-18 04:54:53 +0000

    Set up landing page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,25 @@
-import altair as alt
-import numpy as np
-import pandas as pd
+import os
 import streamlit as st
+from PIL import Image
 
+st.set_page_config(
+    page_title="Generative Manim",
+)
+
+styl = f"""
+<style>
+  textarea[aria-label="Code generated: "] {{
+    font-family: 'Consolas', monospace !important;
+  }}
+  }}
+</style>
 """
-# Welcome to Streamlit!
+st.markdown(styl, unsafe_allow_html=True)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.title(":art: Generative Manim")
+st.write(":robot_face: Create beautiful and quick animations with GPT-4 and GPT-3.5 :sparkles:")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+st.write("## Hello! The new demo it's on [GM Demo](https://generative-manim.vercel.app) :rocket:")
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+st.write('Made with :heart: by [Marcelo](https://github.com/360macky).')
+st.write('[Source code](https://github.com/360macky/generative-manim) - [Report a bug](https://github.com/360macky/generative-manim/issues/new) - [Twitter](https://twitter.com/360macky) - [OpenAI Profile](https://community.openai.com/u/360macky/summary)')
